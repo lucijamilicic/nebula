6db7b0dd8986f7d146065de84b2119eb3ef1f6ae
Natalija9 <asanovic.natalija@gmail.com>
2021-05-01 23:36:04 +0200

    changeLocation - textField

--- flutter_app/lib/screens/changeLocation.dart
+++ flutter_app/lib/screens/changeLocation.dart
@@ -11,14 +11,79 @@ import 'package:flutter_app/weatherModel.dart';
 import 'resizingScreens.dart';
 
 
-
-
-
 class ChangeLocation extends StatelessWidget {
 
   static Future<void> displayText() async {
     print('zaaaaastava');
   }
+
+  @override
+  Widget build(BuildContext context) {
+    // configure aspect ratio depending on screen size
+    SizeConfig.init(context);
+
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(''),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      drawer: MainDrawer(),
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/location.jpg',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.black38),
+            ),
+            Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: SizeConfig.blockSizeVertical! * 10),
+                          Text(
+                            'Enter location: ',
+                            style: GoogleFonts.lato(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: SizeConfig.blockSizeVertical! * 5),
+                          TextField(
+                              obscureText: true,
+                              autofocus: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Enter a new location',
+                              ),
+                          )
+                        ]
+
+                    ),
+                  ],
+                ),
+            ),
+        ],
+        ),
+      ),
+    );
+  }
+
+/*
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -53,5 +118,5 @@ class ChangeLocation extends StatelessWidget {
       ),
     );
   }
-
+  */
 }
